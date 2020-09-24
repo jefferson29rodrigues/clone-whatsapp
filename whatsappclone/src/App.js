dc436066dd7a029688042299756c81094a73d180
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-09-24 19:07:24 -0300

    chat a esquerda e direita e barra de rolagem levando ao final ok

--- whatsappclone/src/App.js
+++ whatsappclone/src/App.js
@@ -95,12 +95,17 @@ export default () => {
     }
   ]);
   const [activeChat, setActiveChat] = useState({});
+  const [user, setUser] = useState({
+    id: 1234,
+    avatar: 'https://www.w3schools.com/w3images/avatar5.png',
+    name: 'Jefferson Rodrigues'
+  });
 
   return (
     <div className="app-window">
       <div className="sidebar">
         <header>
-          <img className="header--avatar" src="https://www.w3schools.com/w3images/avatar4.png" />
+          <img className="header--avatar" src={user.avatar} alt="" />
           <div className="header--buttons">
             <div className="header--btn">
               <DonutLargeIcon style={{color: '#919191'}} />
@@ -132,7 +137,9 @@ export default () => {
       </div>
       <div className="contentarea">
         {activeChat.chatId !== undefined &&
-          <ChatWindow />
+          <ChatWindow 
+            user={user}
+          />
         }
         {activeChat.chatId === undefined &&
           <ChatIntro />
